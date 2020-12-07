a227a1ced49a49f178dd57cad5906a2b242b0ed9
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2020-12-07 11:21:13 +0530

    done Ex08

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 var text = "Welcome";
 var userName = "NeoGrammer";
-var likeCounter = 0;
+// var likeCounter = 0;
+
 export default function App() {
+  const [likeCounter, setLikeCounter] = useState(0);
+
   function likeHandler() {
-    console.log("clicked!" + likeCounter);
-    likeCounter = likeCounter + 1;
+    // console.log("clicked!" + likeCounter);
+    var NewlikeCounter = likeCounter + 1;
+    setLikeCounter(NewlikeCounter);
   }
+  console.log(likeCounter);
   return (
     <div className="App">
       <h1>
         {text} {userName}
       </h1>
-      <button onClick={likeHandler}>Like</button>
+      <button onClick={likeHandler}>Like</button> {likeCounter}
     </div>
   );
 }
