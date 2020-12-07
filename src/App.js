7c461b51a72a5aeb6d9fb5e069bfae2de4cc67e1
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2020-12-07 10:30:05 +0530

    done Ex06 Ex07

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import "./styles.css";
 
 var text = "Welcome";
 var userName = "NeoGrammer";
+var likeCounter = 0;
 export default function App() {
+  function likeHandler() {
+    console.log("clicked!" + likeCounter);
+    likeCounter = likeCounter + 1;
+  }
   return (
     <div className="App">
       <h1>
         {text} {userName}
       </h1>
+      <button onClick={likeHandler}>Like</button>
     </div>
   );
 }
