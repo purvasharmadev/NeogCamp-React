99589969685e58756bd5b6842e49fcb708fbebd1
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2020-12-07 10:06:35 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./styles.css";
+
+var text = "inside outtt";
+var color = "red";
+export default function App() {
+  return (
+    <div className="App">
+      <h1>{text}</h1>
+    </div>
+  );
+}
+
+/**
+ * things to notice
+ * class --> className
+ * style --> takes an object instead of ""
+ */
